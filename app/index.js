198000e03b1c4e1d2c8f9a95be983b36fa52f2f6
oswalde-p <31041273+oswalde-p@users.noreply.github.com>
2020-12-06 14:36:44 +1100

    Use npm gurhli core (#19)
    * WIP
    
    * Use guhrliApp functions
    
    * Catch GuhrliErrors
    
    * WIP pass CGM settings to module
    
    * Use package from npmjs registry

--- app/index.js
+++ app/index.js
@@ -5,10 +5,11 @@ import { me as device } from 'device'
 import { vibration } from 'haptics'
 import { peerSocket } from 'messaging'
 
+import guhrliApp from 'fitbit-guhrli-core/app'
+
 import { formatDate, getTimeStr, round } from '../common/utils'
 import { LOW_BATTERY_LIMIT } from '../common/constants'
 import { Settings } from './settings'
-import { Guhrli } from './lib/guhrli-core-app'
 
 // Update the clock every minute
 clock.granularity = 'minutes'
@@ -26,7 +27,16 @@ const sgvAgeText = document.getElementById('reading-age')
 
 batteryStatusText.text = 'init'
 
-const gurhli = new Guhrli()
+try {
+  guhrliApp.initialize(peerSocket)
+} catch(err) {
+  if (err instanceof guhrliApp.GuhrliError) {
+    console.error('Error initializing guhrliApp')
+    console.error(err)
+  } else {
+    throw err
+  }
+}
 
 function onTick(evt) {
   let now = evt ? evt.date : new Date()
@@ -127,10 +137,20 @@ function clearAlert(key) {
 }
 
 function updateReading() {
-  sgvText.text = gurhli.reading
-  // this should be done by adding classes but I can't work out how to do that
-  timeText.style.fill = colorMap[gurhli.alarm] || colorMap.default
-  sgvAgeText.text = gurhli.formattedAge() || ''
+  try {
+    sgvText.text = guhrliApp.getReading()
+    // this should be done by adding classes but I can't work out how to do that
+    timeText.style.fill = colorMap[guhrliApp.getAlarm()] || colorMap.default
+    sgvAgeText.text = guhrliApp.getFormattedAge() || ''
+
+  } catch (err) {
+    if (err instanceof guhrliApp.GuhrliError) {
+      console.error('Error displaying guhrli reading')
+      console.error(err)
+      return
+    }
+    throw err
+  }
 }
 
 const colorMap = {
--- companion/index.js
+++ companion/index.js
@@ -1,5 +1,54 @@
+import { settingsStorage } from 'settings'
 import * as simpleSettings from './simple/companion-settings'
-import * as GuhrliCompanion from './lib/guhrli-core-companion'
+import { initialize, GuhrliError } from 'fitbit-guhrli-core/companion'
 
 simpleSettings.initialize()
-GuhrliCompanion.initialize()
+
+// order is defined by the Settings component
+const BG_SOURCES = [
+  'NONE',
+  'NIGHTSCOUT',
+  'TOMATO',
+  'XDRIP'
+]
+
+const KEYS = {
+  BG_SOURCE: 'bgSource',
+  NIGHTSCOUT_URL: 'nightscoutUrl'
+}
+
+function parseSettings() {
+  const res = JSON.parse(settingsStorage.getItem(KEYS.BG_SOURCE))
+  const source = res.selected ? BG_SOURCES[res.selected[0]] : 'NONE'
+  const nightscoutSetting = settingsStorage.getItem(KEYS.NIGHTSCOUT_URL)
+  if (nightscoutSetting) {
+    const { name: nightscoutURL } = JSON.parse(nightscoutSetting)
+    return { source, nightscoutURL }
+  }
+  return { source }
+}
+
+updateGuhrli()
+
+// don't forget to listen for settings updates...
+settingsStorage.addEventListener('change', (evt) => {
+  if (evt.key == KEYS.BG_SOURCE || evt.key == KEYS.NIGHTSCOUT_URL){
+    updateGuhrli()
+  }
+})
+
+function updateGuhrli() {
+  // first, read current settings from storage
+  const currentSettings = parseSettings()
+  if (!currentSettings.source) return
+  // now initialize the service
+  try {
+    initialize(currentSettings)
+  } catch(err) {
+    if (err instanceof GuhrliError) {
+      console.error('Error initializing Guhrli companion') // eslint-disable-line no-console
+      console.error(err) // eslint-disable-line no-console
+    }
+    throw (err)
+  }
+}
